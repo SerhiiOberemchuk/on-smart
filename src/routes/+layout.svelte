8b25847aa7a8a5264d04c81cab11e4fdc0503f5f
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-10-03 20:41:10 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '../components/layoute/Header.svelte';
 	import Footer from '../components/layoute/Footer.svelte';
-
+	import type { Config } from '@sveltejs/adapter-vercel';
+	export const config: Config = {
+		runtime: 'nodejs22.x'
+	};
 	let { children } = $props();
 </script>
 
