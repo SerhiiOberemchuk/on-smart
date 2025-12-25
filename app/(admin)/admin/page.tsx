c4d04f093f92ae201f6a9a562786451c0cb1208f
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2025-12-25 22:27:07 +0100

    Update page.tsx

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -5,7 +5,6 @@ import { headers } from "next/headers";
 
 export default async function AdminMainPage() {
   const session = await auth.api.getSession({ headers: await headers() });
-
   return (
     <div className="flex h-svh flex-col items-center justify-center gap-6">
       <h1>Welcome, {session?.user.name}</h1> <p>Admin {session?.user.email}</p>
