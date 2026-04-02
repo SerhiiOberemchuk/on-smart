a466531017ccb3b1024532937f064e1366baf528
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2026-04-02 15:53:48 +0200

    - proxy

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,11 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
+// import { auth } from "@/lib/auth";
 // import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (request.nextUrl.pathname.startsWith("/admin/auth")) {
-    return NextResponse.next();
-  }
-
-  if (!session || session.user.role !== "admin") {
-    return NextResponse.redirect(new URL("/admin/auth", request.url));
-  }
+  // void request;
+  // void auth;
+  // void headers;
 
   return NextResponse.next();
 }
